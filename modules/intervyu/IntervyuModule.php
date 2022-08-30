6dca1fc562ff9a865850cdb14f5ac101763cb717
Oybek <on.1349@list.ru>
2022-08-30 18:42:49 +0500

    first commit

---
+++ modules/intervyu/IntervyuModule.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace app\modules\intervyu;
+
+/**
+ * intervyu module definition class
+ */
+class IntervyuModule extends \yii\base\Module
+{
+    /**
+     * {@inheritdoc}
+     */
+    public $controllerNamespace = 'app\modules\intervyu\controllers';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
